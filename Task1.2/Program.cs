eae6f7f5e3100ebc5aed373bb91d9aec4a860f77
Gergana Koleva <gerganakoleva03@schoolmath.eu>
2024-04-22 15:14:08 +0300

    Add Task 1.2

---
+++ Task1.2/Program.cs
@@ -0,0 +1,28 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Input the first 9 digits of the ISBN: ");
+        string input = Console.ReadLine();
+
+        string isbn = input + CalculateCheckDigit(input);
+        Console.WriteLine(isbn);
+    }
+
+    public static string CalculateCheckDigit(string input)
+    {
+        int length = input.Length;
+        int inputNum = int.Parse(input);
+        int sum = 0;
+
+        for (int i = 0; i < length; i++)
+        {
+            sum += (inputNum / (int)Math.Pow(10, length - 1 - i)) * (length + 1 - i);
+            inputNum = inputNum % (int)Math.Pow(10, length - 1 - i);
+        }
+
+        int checkDigit = 11 - (sum % 11);
+
+        return checkDigit == 10 ? "X" : checkDigit.ToString();
+    }
+}
