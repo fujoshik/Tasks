61a6bea9baf718b2d195e602920032b05b8c09e4
Gergana Koleva <gerganakoleva03@schoolmath.eu>
2024-04-22 16:54:00 +0300

    Add Task 1.3

---
+++ Task1.3/Program.cs
@@ -0,0 +1,49 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Input array:");
+        var input = Console.ReadLine().Split(", ").ToArray();
+
+        var array = new int[input.Length];
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = int.Parse(input[i]);
+        }
+        Console.WriteLine("Array: " + string.Join(" ", array));
+
+        var uniqueIntegers = FindUniqueValues(array);
+
+        Console.Write("Unique integers: ");
+        for (int i = 0; i < uniqueIntegers.Length; i++)
+        {
+            Console.Write($"{uniqueIntegers[i]} ");
+        }
+    }
+
+    public static int[] FindUniqueValues(int[] array)
+    {
+        int[] uniqueIntegers = new int[array.Length];
+        int uniqueIntegersCount = 0;
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            int j;
+            for (j = 0; j < i; j++)
+            {
+                if (array[i] == array[j])
+                {
+                    break;
+                }
+            }
+            if (i == j)
+            {
+                uniqueIntegers[uniqueIntegersCount] = array[i];
+                uniqueIntegersCount++;
+            }
+        }
+
+        return uniqueIntegers;
+    }
+}
