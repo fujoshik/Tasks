833da67b8877cd5992bf2f62e1e2fc045bfd8eca
Gergana Koleva <gerganakoleva03@schoolmath.eu>
2024-04-30 12:04:56 +0300

    Add Task2.2 & unit tests

---
+++ Task 2/Task2.2/DiagonalMatrixExtensions.cs
@@ -0,0 +1,29 @@
+﻿namespace Task2._2
+{
+    public static class DiagonalMatrixExtensions
+    {
+        public static DiagonalMatrix Add(this DiagonalMatrix a, DiagonalMatrix b)
+        {
+            int biggerSize = int.Max(a.Size, b.Size);
+            var newMatrix = new int[biggerSize];
+
+            for (int i = 0; i < biggerSize; i++)
+            {
+                if (i < a.Size && i < b.Size)
+                {
+                    newMatrix[i] = a.Elements[i] + b.Elements[i];
+                }
+                else if (i < a.Size)
+                {
+                    newMatrix[i] = a.Elements[i];
+                }
+                else
+                {
+                    newMatrix[i] = b.Elements[i];
+                }
+            }
+
+            return new DiagonalMatrix(newMatrix);
+        }
+    }
+}
--- Task 2/Task2.2/Program.cs
+++ Task 2/Task2.2/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Task2._2;
+
+var matrix1 = new DiagonalMatrix(1, 2, 3);
+Console.WriteLine(matrix1.ToString());
+Console.WriteLine(matrix1[1, 5]);
+Console.WriteLine();
+
+var matrix2 = new DiagonalMatrix(4, 5, 6, 7);
+Console.WriteLine(matrix2.ToString());
+Console.WriteLine();
+
+Console.WriteLine("Adding matrices:");
+Console.WriteLine(matrix1.Add(matrix2));
