70b74c4fa7ed17006a9efb5561aa38aa16e34b2d
Gergana Koleva <gerganakoleva03@schoolmath.eu>
2024-05-15 01:01:42 +0300

    Add Task4.1 & unit tests

---
+++ Task 4/Task4.1/GenericDiagonalMatrix.cs
@@ -0,0 +1,44 @@
+﻿namespace Task4._1
+{
+    public class GenericDiagonalMatrix<T>
+    {
+        public event EventHandler<GenericMatrixElementChangedEventArgs<T>>? ElementChanged;
+
+        private T[] _elements;
+        public int Size { get; private set; }
+
+        public GenericDiagonalMatrix(int size)
+        {
+            if (size < 0)
+            {
+                throw new ArgumentException("Size must be a positive number!");
+            }
+            Size = size;
+            _elements = new T[size];
+        }
+
+        public T this[int i, int j]
+        {
+            get
+            {
+                if (i < 0 || j < 0 || i >= Size || j >= Size)
+                {
+                    throw new IndexOutOfRangeException("Indexes must be greater than 0 and less than the size of the matrix");
+                }
+                return (i == j && i >= 0 && i < Size) ? _elements[i] : default;
+            }
+            set
+            {
+                if (!(i == j && i >= 0 && i < Size))
+                {
+                    throw new IndexOutOfRangeException("Indexes must be greater than 0 and less than the size of the matrix");
+                }
+                if (!value.Equals(_elements[i]))
+                {               
+                    ElementChanged?.Invoke(this, new GenericMatrixElementChangedEventArgs<T>(i, j, _elements[i], value));
+                    _elements[i] = value;
+                }
+            }
+        }
+    }
+}
---
+++ Task 4/Task4.1/Program.cs
@@ -0,0 +1,20 @@
+﻿using Task4._1;
+
+var matrix1 = new GenericDiagonalMatrix<int>(3);
+matrix1[0,0] = 1;
+matrix1[1,1] = 1;
+matrix1[2,2] = 1;
+
+var matrix2 = new GenericDiagonalMatrix<int>(4);
+matrix2[0, 0] = 2;
+matrix2[1, 1] = 2;
+matrix2[2, 2] = 2;
+matrix2[3, 3] = 2;
+
+Console.WriteLine("Adding matrices:");
+var result = matrix1.Add(matrix2, (x, y) => x+y);
+Console.WriteLine();
+
+var tracker = new MatrixTracker<int>(result);
+result[3, 3] = 10;
+tracker.Undo();
