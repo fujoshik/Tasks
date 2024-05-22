e7d4e6c257617fd89a5f31b18978587d48f929ae
Gergana Koleva <gerganakoleva03@schoolmath.eu>
2024-05-22 15:23:15 +0300

    Add Task 5.2

---
+++ Task 5.2/Task5.2/Entities/Book.cs
@@ -0,0 +1,26 @@
+﻿namespace Task5._2.Entities
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public DateTime? PublicationDate { get; set; }
+        public List<string> Authors { get; set; }
+
+        public Book(string title)
+        {
+            if (string.IsNullOrEmpty(title))
+            {
+                throw new ArgumentNullException(nameof(title));
+            }
+            Title = title;
+            Authors = new List<string>();
+        }
+
+        public Book(string title, DateTime publicationDate, IList<string> authors)
+            : this(title)
+        {
+            PublicationDate = publicationDate;
+            Authors = authors.Distinct().ToList();
+        }
+    }
+}
---
+++ Task 5.2/Task5.2/Entities/Catalog.cs
@@ -0,0 +1,74 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Task5._2.Entities
+{
+    public class Catalog
+    {
+        private Dictionary<string, Book> _books;
+
+        public Catalog()
+        {
+            _books = new Dictionary<string, Book>();
+        }
+
+        public void AddBook(string isbn, Book book)
+        {
+            if (book == null)
+            {
+                throw new ArgumentNullException(nameof(book));
+            }
+
+            if (!((isbn.Count(x => x == '-') == 4 && isbn.Length == 17) || (isbn.Length == 13 && !isbn.Contains("-"))))
+            {
+                throw new ArgumentException(nameof(isbn));             
+            }
+
+            string cleanIsbn = isbn.Replace("-", "");
+            _books.Add(cleanIsbn, book);
+        }
+
+        public Book GetBook(string isbn)
+        {
+            string cleanIsbn = isbn.Replace("-", "");
+
+            if (_books.TryGetValue(cleanIsbn, out Book book))
+            {
+                return book;
+            }
+
+            return null;
+        }
+
+        public IList<string> GetBookTitles()
+        {
+            return _books
+                .OrderBy(x => x.Value.Title)
+                .Select(x => x.Value.Title)
+                .ToList();
+        }
+
+        public IList<Book> GetBooksByAuthor(string author)
+        {
+            if (string.IsNullOrEmpty(author))
+            {
+                throw new ArgumentNullException(author);
+            }
+
+            return _books
+                .Where(x => x.Value.Authors.Contains(author))
+                .OrderBy(x => x.Value.PublicationDate)
+                .Select(x => x.Value)
+                .ToList();
+        }
+
+        public IList<(string, int)> GetSetAuthorBooksCount()
+        {
+            return _books
+                .SelectMany(x => x.Value.Authors)
+                .GroupBy(a => a)
+                .Select(group => (Author: group.Key, Count: group.Count()))
+                .OrderBy(x => x.Author)
+                .ToList();
+        }
+    }
+}
