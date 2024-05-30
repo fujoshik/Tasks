45c2fb63cf5a331728125101b8b8bbf70b2b18d2
Gergana Koleva <gerganakoleva03@schoolmath.eu>
2024-05-30 12:19:03 +0300

    Add changes

--- Task 5.1/Task5.1/SparseMatrix.cs
+++ Task 5.1/Task5.1/SparseMatrix.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections;
+using System.Text;
+using System.Xml.Linq;
 
 namespace Task5._1
 {
@@ -45,9 +47,16 @@ namespace Task5._1
 
                 if (_elements.ContainsKey(key))
                 {
-                    _elements[key] = value;
+                    if (value != 0)
+                    {
+                        _elements[key] = value;
+                    }
+                    else
+                    {
+                        _elements.Remove(key);
+                    }
                 }
-                else
+                else if (value != 0)
                 {
                     _elements.Add(key, value);
                 }
@@ -56,20 +65,17 @@ namespace Task5._1
 
         public override string ToString()
         {
-            string result = "Elements: ";
+            StringBuilder result = new StringBuilder("Elements: ");
 
             for (int i = 0; i < Rows; i++)
             {
                 for (int j = 0; j < Columns; j++)
                 {
-                    if (_elements[new Tuple<int,int>(i, j)] != 0)
-                    {
-                        result += $"{_elements[new Tuple<int, int>(i, j)]} ";
-                    }                      
+                    result.Append($"{_elements[new Tuple<int, int>(i, j)]} ");
                 }
             }
 
-            return result;
+            return result.ToString();
         }
 
         public IEnumerator<long> GetEnumerator()
@@ -90,36 +96,19 @@ namespace Task5._1
 
         public IEnumerable<(int,int,long)> GetNonzeroElements()
         {
-            var result = new List<(int,int,long)>();
-            for (int i = 0; i < Columns; i++)
-            {
-                for (int j = 0; j < Rows; j++)
-                {
-                    if (_elements[new Tuple<int, int>(j, i)] != 0)
-                    {
-                        result.Add((j, i, _elements[new Tuple<int, int>(j, i)]));
-                    }
-                }
-            }
-
-            return result;
+            return _elements
+                .OrderBy(x => x.Key.Item2)
+                .ThenBy(x => x.Key.Item1)
+                .Select(x => (x.Key.Item1, x.Key.Item2, x.Value));
         }
 
         public int GetCount(int x)
         {
-            int count = 0;
-            for (int i = 0; i < Rows; i++)
+            if (x == 0)
             {
-                for (int j = 0; j < Columns; j++)
-                {
-                    if (_elements[new Tuple<int,int>(i, j)] == x)
-                    {
-                        count++;
-                    }
-                }
-            }
-
-            return count;
+                return Rows * Columns - _elements.Count;
+            }            
+            return _elements.Count(e => e.Value == x);
         }
     }
 }
